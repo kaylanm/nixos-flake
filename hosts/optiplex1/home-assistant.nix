0d4fc9d024bfa593065c32017b675a316e6c63fc
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2024-07-07 11:10:22 -0400

    Add additional home-assistant config.

--- hosts/optiplex1/home-assistant.nix
+++ hosts/optiplex1/home-assistant.nix
@@ -9,11 +9,15 @@
         image = "ghcr.io/home-assistant/home-assistant:2024.7.1";
         extraOptions = [
           "--network=host"
+          "--device=/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_6a21b96f1fe8ec118b107a60e89bdf6f-if00-port0"
         ];
         volumes = [
           "/var/lib/home-assistant:/config"
+          "/var/lib/ssl:/ssl"
         ];
       };
     };
   };
+
+  networking.firewall.allowedTCPPorts = [ 443 ];
 }
