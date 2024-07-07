1d6e67f79b34b3a6a56911c0fb424ca813fe1486
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2024-07-07 09:52:06 -0400

    Add home-assistant.

--- hosts/optiplex1/home-assistant.nix
+++ hosts/optiplex1/home-assistant.nix
@@ -6,7 +6,7 @@
     containers = {
       homeAssistant = {
         environment.TZ = "America/New_York";
-        image = "ghcr.io/home-assistant/home-assistant:stable";
+        image = "ghcr.io/home-assistant/home-assistant:2024.7.1";
         extraOptions = [
           "--network=host"
         ];
