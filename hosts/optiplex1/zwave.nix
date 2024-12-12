e6bad36e51137851538de418486cfa6f8a98f10e
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2024-12-12 16:16:23 -0500

    Override zwave-js-server.

--- hosts/optiplex1/zwave.nix
+++ hosts/optiplex1/zwave.nix
@@ -5,6 +5,7 @@
     enable = true;
     serialPort = "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_9e8433102294eb118a4a32703d98b6d1-if00-port0";
     secretsConfigFile = "/etc/zwave-secrets.json";
-    package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.zwave-js-server;
+    # package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.zwave-js-server;
+    package = pkgs.callPackage (import ./zwave-js-server.nix) {};
   };
 }
