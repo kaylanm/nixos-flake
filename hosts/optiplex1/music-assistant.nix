29b9a290d6f9556ee498bb557e77afdeaa68b332
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-01-17 21:12:48 +0000

    Switch music-assistant to container.

--- hosts/optiplex1/music-assistant.nix
+++ hosts/optiplex1/music-assistant.nix
@@ -1,30 +1,23 @@
 { config, pkgs, inputs, ... }:
 
 {
-  services.music-assistant = {
-    enable = true;
-    #package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.music-assistant;
-    providers = [
-      "airplay"
-      "apple_music"
-      "builtin"
-      "chromecast"
-      "dlna"
-      "filesystem_local"
-      "filesystem_smb"
-      "fully_kiosk"
-      "hass"
-      "hass_players"
-      "musicbrainz"
-      "opensubsonic"
-      #"player_group"
-      "radiobrowser"
-      "slimproto"
-      "snapcast"
-      "soundcloud"
-      "spotify"
-      "theaudiodb"
-      "ytmusic"
-    ];
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      homeAssistant = {
+        image = "ghcr.io/music-assistant/server:2.3.6";
+        extraOptions = [
+          "--network=host"
+          # "--cap-add=DAC_READ_SEARCH"
+          # "--cap-add=SYS_ADMIN"
+          # "--security-opt apparmor:unconfined"
+        ];
+        volumes = [
+          "/var/lib/music-assistant:/data"
+        ];
+      };
+    };
   };
+
+  networking.firewall.allowedTCPPorts = [ 3483 8095 8096 8097 ];
 }
