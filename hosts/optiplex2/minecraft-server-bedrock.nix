792392354464a3a508dd8ab8d7bdb0af122e1578
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-01-18 12:13:03 -0500

    Add Jack to bedrock server.

--- hosts/optiplex2/minecraft-server-bedrock.nix
+++ hosts/optiplex2/minecraft-server-bedrock.nix
@@ -10,8 +10,9 @@
         SERVER_NAME = "Ryan Minecraft";
         TZ = "US/Eastern";
         VERSION = "LATEST";
-        ALLOW_LIST_USERS = "ExInfernum:2533274799590022,Zavazone:2535423916767720,LunarCart175331:2535415429916730";
-        OPS = "2533274799590022,2535415429916730";
+        ALLOW_LIST_USERS =
+        "ExInfernum:2533274799590022,Zavazone:2535423916767720,LunarCart175331:2535415429916730,TulinShark:2535464932198448";
+        OPS = "2533274799590022,2535415429916730,2535464932198448";
       };
       image = "itzg/minecraft-bedrock-server:2024.11.0";
       ports = [ "0.0.0.0:19132:19132/udp" ];
