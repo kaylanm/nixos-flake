923c6a8fecec9d2fb30dbe6e8602da82bf5328f6
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-03-15 12:13:11 -0400

    Cleanup.

--- hosts/auriga-nixos/configuration.nix
+++ hosts/auriga-nixos/configuration.nix
@@ -5,7 +5,6 @@
 
 {
   imports = [
-    # include NixOS-WSL modules
     inputs.nixos-wsl.nixosModules.wsl
   ];
 
@@ -22,11 +21,5 @@
   networking.hostName = "auriga-nixos";
   networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11";
 }
