4c48740415403be3945ea1e502c5dda206a7bdcc
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-03-16 20:25:47 -0400

    Add nix gc.

--- modules/by-name/de/defaults/nixos-module.nix
+++ modules/by-name/de/defaults/nixos-module.nix
@@ -4,6 +4,16 @@
   config = {
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+    gc = {
+      automatic = true;
+      interval = {
+        Weekday = 0;
+        Hour = 0;
+        Minute = 0;
+      };
+      options = "--delete-older-than 30d";
+    };
+
     nixpkgs.config.allowUnfree = true;
 
     # Set your time zone.
