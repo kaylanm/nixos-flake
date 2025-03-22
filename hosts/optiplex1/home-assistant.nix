733a144188a37cea9468804f95cf07619746b311
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-03-22 14:24:13 -0400

    Bump home-assistant.

--- hosts/optiplex1/home-assistant.nix
+++ hosts/optiplex1/home-assistant.nix
@@ -6,7 +6,7 @@
     containers = {
       homeAssistant = {
         environment.TZ = "America/New_York";
-        image = "ghcr.io/home-assistant/home-assistant:2025.3.2";
+        image = "ghcr.io/home-assistant/home-assistant:2025.3.4";
         extraOptions = [
           "--network=host"
           "--device=/dev/serial/by-id/usb-ITead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_6a21b96f1fe8ec118b107a60e89bdf6f-if00-port0"
