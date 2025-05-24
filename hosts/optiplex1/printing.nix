ebcc488dbcdac340ddac985fd7c989f77ff4af9c
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-05-24 08:50:45 -0400

    Remove IPv4 avahi overrides.

--- hosts/optiplex1/printing.nix
+++ hosts/optiplex1/printing.nix
@@ -24,12 +24,6 @@
     };
   };
 
-  system.nssModules = pkgs.lib.optional true pkgs.nssmdns;
-  system.nssDatabases.hosts = pkgs.lib.optionals true (pkgs.lib.mkMerge [
-    (pkgs.lib.mkBefore [ "mdns4_minimal [NOTFOUND=return]" ])
-    (pkgs.lib.mkAfter [ "mdns4" ])
-  ]);
-
   hardware.printers = {
     ensurePrinters = [
       {
