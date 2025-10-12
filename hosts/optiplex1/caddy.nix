04c6f1948ad819622c616c733bedc030f25ae299
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-10-12 17:04:59 -0400

    Update caddy config, grace_period.

--- hosts/optiplex1/caddy.nix
+++ hosts/optiplex1/caddy.nix
@@ -3,18 +3,17 @@
 {
   services.caddy = {
     enable = true;
-    # TODO: Switch when available
-    # environmentFile = "/etc/caddy/environment";
+    environmentFile = "/etc/caddy/environment";
     globalConfig = ''
       tailscale {
         ephemeral true
       }
+
+      grace_period 10s
     '';
     package = pkgsUnstable.caddy.withPlugins {
       plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" ];
       hash = "sha256-sFXQcvAGGt9ulCoiH3vqHPNk+zxgRtCXH4V/0ng/UhQ=";
     };
   };
-
-  systemd.services.caddy.serviceConfig.EnvironmentFile = [ "/etc/caddy/environment" ];
 }
