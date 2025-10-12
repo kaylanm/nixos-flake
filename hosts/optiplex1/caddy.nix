3d39fbc7952563b7d75b575b195d7f7ce36f7dd6
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-10-12 10:14:44 -0400

    Bump caddy hash.

--- hosts/optiplex1/caddy.nix
+++ hosts/optiplex1/caddy.nix
@@ -12,7 +12,7 @@
     '';
     package = pkgsUnstable.caddy.withPlugins {
       plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250207163903-69a970c84556" ];
-      hash = "sha256-TS5Ym3jHa2H2fmwV/QYoZRk7yEE+UWIcyabhhRQQrUM=";
+      hash = "sha256-sFXQcvAGGt9ulCoiH3vqHPNk+zxgRtCXH4V/0ng/UhQ=";
     };
   };
 
