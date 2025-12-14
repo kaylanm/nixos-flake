927b645bf7b651285cafdca946eacad157472896
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-12-14 18:44:51 -0500

    Update restic config for optiplex1.

--- hosts/optiplex1/restic.nix
+++ hosts/optiplex1/restic.nix
@@ -6,6 +6,12 @@
       initialize = true;
       paths = [
         "/var/lib/home-assistant"
+        "/var/lib/immich"
+      ];
+      pruneOpts = [
+        "--keep-daily 14"
+        "--keep-weekly 4"
+        "--keep-monthly 1"
       ];
       repositoryFile = "/etc/restic-repository";
       passwordFile = "/etc/restic-password";
