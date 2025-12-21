f5336b59767936daad062a7512930cc91e971287
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2025-12-21 12:21:20 -0500

    Remove timekpr backport.

--- hosts/elite1/timekpr.nix
+++ hosts/elite1/timekpr.nix
@@ -1,10 +1,6 @@
 { config, pkgs, pkgsUnstable, lib, inputs, ... }:
 
 {
-  imports = [
-    "${inputs.nixpkgs-unstable}/nixos/modules/services/security/timekpr.nix"
-  ];
-
   services.timekpr = {
     enable = true;
     package = pkgsUnstable.timekpr;
