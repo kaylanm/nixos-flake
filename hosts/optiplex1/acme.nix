21e31b2e8dbbe2551a5b55c55d3561512f675c78
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-06-03 19:53:54 -0400

    Update home-assistant cert after renewal.

--- hosts/optiplex1/acme.nix
+++ hosts/optiplex1/acme.nix
@@ -8,6 +8,13 @@
       domain = "*.kaylan.dev";
       dnsProvider = "route53";
       environmentFile = "/etc/acme-env";
+      postRun = ''
+        cp key.pem /var/lib/ssl/privkey.pem
+        cp fullchain.pem /var/lib/ssl/fullchain.pem
+      '';
+      reloadServices = [
+        "docker-homeAssistant.service"
+      ];
     };
   };
 }
