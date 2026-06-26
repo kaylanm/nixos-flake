a16f11693a20f633a63c941009c813bfbd3097db
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-06-26 11:11:29 -0400

    Bump music-assistant.

--- hosts/optiplex1/music-assistant.nix
+++ hosts/optiplex1/music-assistant.nix
@@ -5,7 +5,7 @@
     backend = "docker";
     containers = {
       musicAssistant = {
-        image = "ghcr.io/music-assistant/server:2.9.1";
+        image = "ghcr.io/music-assistant/server:2.9.4";
         extraOptions = [
           "--network=host"
           # "--cap-add=DAC_READ_SEARCH"
