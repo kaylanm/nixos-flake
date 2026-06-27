5b6237f025fc651f03bc351642cbf7e285dec384
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-06-27 11:51:39 -0400

    Switch to new_feature GPU driver.

--- hosts/elite1/configuration.nix
+++ hosts/elite1/configuration.nix
@@ -49,7 +49,8 @@
     nvidiaSettings = true;
     #package = config.boot.kernelPackages.nvidiaPackages.stable;
     # Until nvidia driver fixes Thunderbolt eGPUs (broken on 595.71.05) could also try new_feature.
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    #package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    package = config.boot.kernelPackages.nvidiaPackages.new_feature;
   };
 
   # Disable iGPU
