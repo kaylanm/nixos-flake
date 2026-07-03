dc182264537c337f4dfd77b005fabed20a3550a4
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-07-03 10:16:52 -0400

    Bump zwave-js-server.

--- hosts/optiplex1/zwave.nix
+++ hosts/optiplex1/zwave.nix
@@ -6,14 +6,14 @@
     serialPort = "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_9e8433102294eb118a4a32703d98b6d1-if00-port0";
     secretsConfigFile = "/etc/zwave-secrets.json";
     package = pkgs.zwave-js-server.overrideAttrs (finalAttrs: previousAttrs: {
-      version = "3.8.0";
+      version = "3.10.0";
       src = pkgs.fetchFromGitHub {
         owner = "zwave-js";
         repo = "zwave-js-server";
         rev = finalAttrs.version;
-        hash = "sha256-/rMeQ+OZdWpRgYwZQNwNa7A7PoTft70LtF2yoUBeKJ4=";
+        hash = "sha256-4gyELYM457g0Fam4651nj6Jt7WWI4ldV/2n+ofjRqoc=";
       };
-      npmDepsHash = "sha256-F2wgnJfOHrQYAtVFzu2urey1MPuXdB6+7VWvP9JUHxg=";
+      npmDepsHash = "sha256-R4wZrQS5/9ll2tFrDqijfHyj+S8SaAb9jhzEw09gpq8=";
       npmDeps = pkgs.fetchNpmDeps {
         inherit (finalAttrs) src;
         name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
