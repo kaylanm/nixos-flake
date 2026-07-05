6fb7800c1f75d1c9c048b6d559e8bc24a126f718
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-07-05 14:10:23 -0400

    Switch to 580.142 for elite1.

--- hosts/elite1/configuration.nix
+++ hosts/elite1/configuration.nix
@@ -49,7 +49,20 @@
     nvidiaSettings = true;
     #package = config.boot.kernelPackages.nvidiaPackages.stable;
     # Until nvidia driver fixes Thunderbolt eGPUs (broken on 595.71.05) could also try new_feature.
-    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    #package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
+    #package = config.boot.kernelPackages.nvidiaPackages.new_feature;
+    #package = config.boot.kernelPackages.nvidiaPackages.latest;
+    #package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+    # The driver that worked on 25.11.
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.142";
+      sha256_64bit = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+      sha256_aarch64 = "sha256-jntr88SpTYR648P1rizQjB/8KleBoa14Ay12vx8XETM=";
+      openSha256 = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
+      settingsSha256 = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
+      persistencedSha256 = "sha256-il403KPFAnDbB+dITnBGljhpsUPjZwmLjGt8iPKuBqw=";
+    };
   };
 
   # Disable iGPU
