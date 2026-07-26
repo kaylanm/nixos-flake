923d451f4e89359e37a0f391e048ae95af8689ab
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-07-26 11:00:01 -0400

    Bump home-assistant.

--- hosts/optiplex1/home-assistant.nix
+++ hosts/optiplex1/home-assistant.nix
@@ -6,7 +6,7 @@
     containers = {
       homeAssistant = {
         environment.TZ = "America/New_York";
-        image = "ghcr.io/home-assistant/home-assistant:2026.7.2";
+        image = "ghcr.io/home-assistant/home-assistant:2026.7.4";
         extraOptions = [
           "--network=host"
         ];
