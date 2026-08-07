32d7de3a27bc3ec57b7e47ad8e7577d2f83fe8cd
Michael Kaylan <1063516+kaylanm@users.noreply.github.com>
2026-08-07 14:58:30 -0400

    Switch to zwave-js-ui.

--- hosts/optiplex1/zwave.nix
+++ hosts/optiplex1/zwave.nix
@@ -1,24 +1,8 @@
 { config, pkgs, inputs, ... }:
 
 {
-  services.zwave-js = {
+  services.zwave-js-ui = {
     enable = true;
     serialPort = "/dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_9e8433102294eb118a4a32703d98b6d1-if00-port0";
-    secretsConfigFile = "/etc/zwave-secrets.json";
-    package = pkgs.zwave-js-server.overrideAttrs (finalAttrs: previousAttrs: {
-      version = "3.10.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "zwave-js";
-        repo = "zwave-js-server";
-        rev = finalAttrs.version;
-        hash = "sha256-4gyELYM457g0Fam4651nj6Jt7WWI4ldV/2n+ofjRqoc=";
-      };
-      npmDepsHash = "sha256-R4wZrQS5/9ll2tFrDqijfHyj+S8SaAb9jhzEw09gpq8=";
-      npmDeps = pkgs.fetchNpmDeps {
-        inherit (finalAttrs) src;
-        name = "${finalAttrs.pname}-${finalAttrs.version}-npm-deps";
-        hash = finalAttrs.npmDepsHash;
-      };
-    });
   };
 }
